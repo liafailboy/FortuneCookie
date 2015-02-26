9971611319e650e5d4d48b744ab4a406034782ba
tommy03b <tommy03b@gmail.com>
2015-02-25 20:50:31 -0500

    added MyPref.java

---
+++ src/MyPref.java
@@ -0,0 +1,57 @@
+
+public class MyPref implements IMyPref {
+
+	@Override
+	public void setMyName(String stringOfName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getMyName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMyEmail(String stringOfEmail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getMyEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMyUserId(String stringOfUserId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getMyUserId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPerson(Person person) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removePerson(Person person) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Person[] getArrayOfPerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
