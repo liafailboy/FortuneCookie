d6c19833684171f55741bc6b0ae6c6d871bf018f
Shotaro Watanabe <liafailboy@gmail.com>
2015-02-27 00:07:14 -0500

    Deleted unused method

--- src/MyPref.java
+++ src/MyPref.java
@@ -21,37 +21,31 @@ public class MyPref implements IMyPref {
 
 	@Override
 	public void setMyName(String stringOfName) {
-		// TODO Auto-generated method stub
 		myName = stringOfName;
 	}
 
 	@Override
 	public String getMyName() {
-		// TODO Auto-generated method stub
 		return myName;
 	}
 
 	@Override
 	public void setMyEmail(String stringOfEmail) {
-		// TODO Auto-generated method stub
 		myEmail = stringOfEmail;
 	}
 
 	@Override
 	public String getMyEmail() {
-		// TODO Auto-generated method stub
 		return myEmail;
 	}
 
 	@Override
 	public void setMyUserId(String stringOfUserId) {
-		// TODO Auto-generated method stub
 		myUserID = stringOfUserId;
 	}
 
 	@Override
 	public String getMyUserId() {
-		// TODO Auto-generated method stub
 		return myUserID;
 	}
 
@@ -63,15 +57,6 @@ public class MyPref implements IMyPref {
 	@Override
 	public void removePerson(IPerson person) {
 		// TODO Auto-generated method stub
-	}
-	
-	/* This method should return the array of IPerson with the specified order
-	 * Compare the level of the person in the array and make the order
-	 * from highest to lowest.
-	 * You should call this method every time after you added or removed person from the array
-	 */
-	private IPerson[] arrangeArrayOfPerson() {
-		return null;
 	}
 
 	@Override
--- src/Person.java
+++ src/Person.java
@@ -18,32 +18,26 @@ public class Person implements IPerson {
 	
 	@Override
 	public String getUserId() {
-		// TODO Auto-generated method stub
 		return userID;
 	}
 	@Override
 	public String getUserEmail() {
-		// TODO Auto-generated method stub
 		return userEmail;
 	}
 	@Override
 	public String getUserName() {
-		// TODO Auto-generated method stub
 		return userName;
 	}
 	@Override
 	public int getPersonLevel() {
-		// TODO Auto-generated method stub
 		return myUserLevel + personUserLevel * 2;
 	}
 	@Override
 	public void setMyUserLevel(int myLevel) {
-		// TODO Auto-generated method stub
 		myUserLevel = myLevel;
 	}
 	@Override
 	public void setPersonUserLevel(int personLevel) {
-		// TODO Auto-generated method stub
 		personUserLevel = personLevel;
 	}
 	
